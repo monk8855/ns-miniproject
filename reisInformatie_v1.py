0bcc5804921c47e838be08118d070ee6ffd393dd
monk8855 <bobspelen@gmail.com>
2016-11-09 14:16:47 +0100

    reisInformatie-V1.1FINAL
    Afgemaakt product met wijzigingen:
    - Design verbeterd
    - Scrollbar toegevoegd
    - Logo toegevoegd
    - Exceptie voor logo ingevoegd

--- reisInformatie_v1.py
+++ reisInformatie_v1.py
@@ -2,6 +2,8 @@ import requests, xmltodict
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+# Pillow inladen
+from PIL import Image, ImageTk
 
 stationsLijst = []
 columns = ("Tijd", "Vervoerder", "Soort trein", "Bestemming", "Spoor")
@@ -16,27 +18,25 @@ except FileNotFoundError:
     messagebox._show("Fout!", "machineLocatie.csv niet gevonden!", _icon="error")
     quit()
 
+
 # Class voor de buttons en frame
 class ReisInformatie:
+
     def __init__(self, master):
         frame = Frame(master)
-        frame.grid(row=2, column=2)
-
+        frame.grid(row=3, column=2, sticky=S)
 
         self.loadButton = Button(frame, text="Haal tijden op", command=getVertrekTijden)
         self.loadButton.pack(side=LEFT)
         self.loadButton.configure(font=("Arial", 15, "bold"))
+        self.loadButton.config(background="#003399")
+        self.loadButton.config(foreground="white")
 
         self.quitButton = Button(frame, text="Stoppen", command=frame.quit)
         self.quitButton.pack(side=RIGHT)
         self.quitButton.configure(font=("Arial", 15, "bold"))
-
         self.quitButton.config(background="red")
         self.quitButton.config(foreground="white")
-
-
-        self.loadButton.config(background="#003399")
-        self.loadButton.config(foreground="white")
 
 
 # Functie die de stationsLijst vult.
@@ -48,6 +48,7 @@ def getStationsLijst():
         if naam['Land'] == "NL":
             langeNaam = naam['Namen']['Lang']
             stationsLijst.append(langeNaam)
+
 
 # Table fill functie
 def getVertrekTijden():
@@ -98,17 +99,28 @@ getStationsLijst()
 station = StringVar(root)
 station.set(standaardStation)
 beginStation = ttk.Combobox(root, textvariable=station, font=('Arial', 13))
-beginStation.grid(row=1, column=2)
+beginStation.grid(row=2, column=2)
 beginStation['values'] = stationsLijst
 tree = ttk.Treeview(columns=columns, show="headings")
-tree.grid(row=1, column=1)
+tree.grid(row=1, column=1, rowspan=3)
+scrollbar = Scrollbar(root)
+scrollbar.grid(sticky=NE + S,row=1,column=1, rowspan=3)
+scrollbar.config(command = tree.yview )
+tree.configure(yscrollcommand=scrollbar.set)
 root.configure(background='#fece22')
+# Voor NS logo
+try:
+    image = Image.open("NS-thumb.png")
+    photo = ImageTk.PhotoImage(image)
+    label = Label(image=photo, background='#fece22')
+    label.grid(row=1, column=2)
+except FileNotFoundError:
+    pass
+
 b = ReisInformatie(root)
 tree.tag_configure('blauw', background='#D7E2E6', foreground='#003399',font=('Arial', 13))
 tree.tag_configure('wit', background='white', foreground='#003399',font=('Arial', 13))
 tree.tag_configure('rood', background='red', foreground='white',font=('Arial', 13))
 getVertrekTijden()
+
 root.mainloop()
-#NS logo toevoegen
-
-
